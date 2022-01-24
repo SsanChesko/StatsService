0dab26abd3ca5d22cb504558bb9cee3f10fed762
Aleksandr Shalagin <ssanchesko@gmail.com>
2022-01-24 16:20:45 +0500

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,64 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int calculateSum(int[] sales) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public int averageSum(int[] sales) {
+        return calculateSum(sales) / sales.length;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int totalMonOverAvr(int[] sales) {
+        int month = 0;
+        for (int sale : sales) {
+            if (sale > averageSum(sales)) {
+                month = month + 1;
+            }
+        }
+        return month;
+    }
+
+    public int totalMonUnderAvr(int[] sales) {
+        int month = 0;
+        for (int sale : sales) {
+            if (sale < averageSum(sales)) {
+                month = month + 1;
+            }
+        }
+        return month;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return maxMonth + 1;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,63 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCalculateSum() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.calculateSum(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldaverageSum() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.averageSum(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.minSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTotalMonOverAvr() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.totalMonOverAvr(sales);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldTotalMonUnderAvr() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.totalMonUnderAvr(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.maxSales(sales);
+        assertEquals(expected, actual);
+    }
+}
